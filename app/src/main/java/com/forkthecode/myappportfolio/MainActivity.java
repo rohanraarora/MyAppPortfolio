7842df20a93a27bb985637b4b16fefca65781c60
Rohan Arora <rohanraarora@gmail.com>
2015-11-19 16:04:58 +0530

    refactor: code change according to review

--- app/src/main/java/com/forkthecode/myappportfolio/MainActivity.java
+++ app/src/main/java/com/forkthecode/myappportfolio/MainActivity.java
@@ -2,8 +2,6 @@ package com.forkthecode.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,49 +13,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.moviesAppButton:
+                makeToast(getString(R.string.popularMoviesApp));
+                break;
+            case R.id.scoresAppButton:
+                makeToast(getString(R.string.scoresApp));
+                break;
+            case R.id.libraryAppButton:
+                makeToast(getString(R.string.libraryApp));
+                break;
+            case R.id.buildAppButton:
+                makeToast(getString(R.string.buildBiggerApp));
+                break;
+            case R.id.readerAppButton:
+                makeToast(getString(R.string.readerApp));
+                break;
+            case R.id.capstoneAppButton:
+                makeToast(getString(R.string.capstoneApp));
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
-
-    public void onSpotifyAppButtonClick(View view){
-        Toast.makeText(this,"This button will launch Spotify Streamer app",Toast.LENGTH_SHORT).show();
-    }
-
-    public void onScoresAppButtonClick(View view){
-        Toast.makeText(this,"This button will launch Scores app",Toast.LENGTH_SHORT).show();
-    }
-
-    public void onLibraryAppBbuttonClick(View view){
-        Toast.makeText(this,"This button will launch Library app",Toast.LENGTH_SHORT).show();
-    }
-
-    public void onBuildAppButtonClick(View view){
-        Toast.makeText(this,"This button will launch Build It Bigger app",Toast.LENGTH_SHORT).show();
-    }
-
-    public void onReaderAppButtonClick(View view){
-        Toast.makeText(this,"This button will launch XYZ Reader app",Toast.LENGTH_SHORT).show();
-    }
-
-    public void onCapstoneAppBbuttonClick(View view){
-        Toast.makeText(this,"This button will launch Capstone app",Toast.LENGTH_SHORT).show();
+    private void makeToast(String appName){
+        Toast.makeText(this,"This button will launch " + appName + " app",Toast.LENGTH_SHORT).show();
     }
 }
